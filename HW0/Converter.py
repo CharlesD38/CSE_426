421937804307c8788bba6182d4fb4db9283c4334
CharlesD38 <90854764+CharlesD38@users.noreply.github.com>
2022-08-22 11:08:07 -0400

    First commits HW0

---
+++ HW0/Converter.py
@@ -0,0 +1,25 @@
+
+from traitlets.config import Config
+import nbformat as nbf
+from nbconvert.exporters import PDFExporter
+from nbconvert.preprocessors import TagRemovePreprocessor
+
+# Setup config
+c = Config()
+
+c.TagRemovePreprocessor.remove_input_tags = ('hide_input',)
+c.TagRemovePreprocessor.enabled = True
+# Configure and run out exporter
+c.PDFExporter.preprocessors = ["nbconvert.preprocessors.TagRemovePreprocessor"]
+
+exporter = PDFExporter(config=c)
+exporter.register_preprocessor(TagRemovePreprocessor(config=c),True)
+
+# Configure and run our exporter - returns a tuple - first element with html,
+# second with notebook metadata
+output = PDFExporter(config=c).from_filename(
+        r"C:\MFE\MFE Sem 3\CSE 426\HW\HW0\867002105_HW0.ipynb")
+
+# Write to output html file
+with open(r"C:\MFE\MFE Sem 3\CSE 426\HW\HW0\867002105_HW0.pdf",  "wb") as f:
+    f.write(output[0])
